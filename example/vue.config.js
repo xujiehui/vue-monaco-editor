e1de708f76b060439101719ad8eb7eaca6e5b6a5
徐杰辉 <>
2021-05-07 16:20:33 +0800

    add docs

--- example/vue.config.js
+++ example/vue.config.js
@@ -1,6 +1,8 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
+  outputDir: '../docs',
+  productionSourceMap: false,
   configureWebpack: config => {
     const plugins = [new MonacoWebpackPlugin()]
     config.plugins.push(...plugins)
